2bc02d4ee36130ce3fff426c95ff10022ddb909b
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-06-23 15:07:10 -0700

    Port testmultiaudio
    Splitting this into two files since we can't have two event loops.

--- test/testmultiaudio2.c
+++ test/testmultiaudio2.c
@@ -20,6 +20,7 @@
 static SDL_AudioSpec spec;
 static Uint8 *sound = NULL;     /* Pointer to wave data */
 static Uint32 soundlen = 0;     /* Length of wave data */
+int devcount = 0;
 
 typedef struct
 {
@@ -53,17 +54,19 @@ play_through_once(void *arg, Uint8 * stream, int len)
 void
 loop()
 {
-    if(cbd[0].done) {
+    for (int i = 0; i < devcount; i++) {
+      if ((!cbd[i].dev) || (cbd[i].done)) {
         emscripten_cancel_main_loop();
         SDL_PauseAudioDevice(cbd[0].dev, 1);
         SDL_CloseAudioDevice(cbd[0].dev);
         SDL_FreeWAV(sound);
         SDL_Quit();
+      }
     }
 }
 
 static void
-test_multi_audio(int devcount)
+test_multi_audio()
 {
     int keep_going = 1;
     int i;
@@ -76,35 +79,50 @@ test_multi_audio(int devcount)
 
     spec.callback = play_through_once;
 
+    SDL_memset(cbd, '\0', sizeof(cbd));
+    SDL_Log("playing on all devices...\n");
     for (i = 0; i < devcount; i++) {
         const char *devname = SDL_GetAudioDeviceName(i, 0);
-        SDL_Log("playing on device #%d: ('%s')...", i, devname);
-        fflush(stdout);
-
-        SDL_memset(&cbd[0], '\0', sizeof(callback_data));
-        spec.userdata = &cbd[0];
-        cbd[0].dev = SDL_OpenAudioDevice(devname, 0, &spec, NULL, 0);
-        if (cbd[0].dev == 0) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Open device failed: %s\n", SDL_GetError());
-        } else {
-            SDL_PauseAudioDevice(cbd[0].dev, 0);
-#ifdef __EMSCRIPTEN__
-            emscripten_set_main_loop(loop, 0, 1);
-#else
-            while (!cbd[0].done);
-                SDL_Delay(100);
-#endif
-            SDL_Log("done.\n");
+        spec.userdata = &cbd[i];
+        cbd[i].dev = SDL_OpenAudioDevice(devname, 0, &spec, NULL, 0);
+        if (cbd[i].dev == 0) {
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Open device %d failed: %s\n", i, SDL_GetError());
         }
     }
+
+    for (i = 0; i < devcount; i++) {
+        if (cbd[i].dev) {
+            SDL_PauseAudioDevice(cbd[i].dev, 0);
+        }
+    }
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loop, 0, 1);
+#else
+    while (keep_going) {
+        keep_going = 0;
+        for (i = 0; i < devcount; i++) {
+            if ((cbd[i].dev) && (!cbd[i].done)) {
+                keep_going = 1;
+            }
+        }
+        SDL_Delay(100);
+    }
+#endif
+    for (i = 0; i < devcount; i++) {
+        if (cbd[i].dev) {
+            SDL_PauseAudioDevice(cbd[i].dev, 1);
+            SDL_CloseAudioDevice(cbd[i].dev);
+        }
+    }
+
+    SDL_Log("All done!\n");
 }
 
 
 int
 main(int argc, char **argv)
 {
-    int devcount = 0;
-
   /* Enable standard application logging */
     SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
 
@@ -129,7 +147,7 @@ main(int argc, char **argv)
             SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load %s: %s\n", argv[1],
                     SDL_GetError());
         } else {
-            test_multi_audio(devcount);
+            test_multi_audio();
             SDL_FreeWAV(sound);
         }
     }
