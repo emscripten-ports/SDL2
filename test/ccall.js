0b2e52fdd5bd86d19157fb373d02c8509e5155ab
Noah Cohn <noahc42@gmail.com>
2021-08-10 12:23:46 -0700

    Update ccall.js

--- test/ccall.js
+++ test/ccall.js
@@ -1,7 +1,4 @@
-let bz=new BroadcastChannel('bez');
-document.getElementById('btn').addEventListener('click',function(){
-bz.postMessage({data: 222});
-});
+
 document.getElementById('di').addEventListener('click',function(){
 Module.ccall("pl");
 });
