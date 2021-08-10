b823ff33f5441c7604948a5a7be2257bba30c010
Noah Cohn <noahc42@gmail.com>
2021-08-10 12:05:39 -0700

    Update filesys.js

--- test/filesys.js
+++ test/filesys.js
@@ -1,7 +1,6 @@
 document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
-document.getElementById("di").click();
 let bz=new BroadcastChannel('bez');
 document.getElementById('btn').addEventListener('click',function(){
 bz.postMessage({data: 222});
@@ -11,7 +10,6 @@ fll.addEventListener('message',ea=> {
 let fill=new Uint8Array(ea.data.data);
 FS.writeFile('/sample.wav',fill);
 console.log("CCalling _pl");
-Module.ccall("pl");
 document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
